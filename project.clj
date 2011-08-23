feb209866e9d57f4f6a2e47426f71809f2a1e746
Mark Rathwell <mark.rathwell@gmail.com>
2011-08-22 22:33:43 -0400

    moved version to 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.rathwell/gaka-plus "1.0.0-SNAPSHOT"
+(defproject org.clojars.rathwell/gaka-plus "1.0.0"
   :description "Extension of the Gaka CSS library for Clojure."
   :dependencies [[org.clojure/clojure "1.2.1"]
 		 [org.clojars.wilkes/gaka "0.2.2"]]

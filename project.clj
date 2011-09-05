4229c58175474c88b4bd99d0ce0ad03fe437f589
Mark Rathwell <mark.rathwell@gmail.com>
2011-09-05 00:55:22 -0400

    bump version to 1.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-style "1.0.0"
+(defproject clj-style "1.0.1"
   :description "A CSS generation library for Clojure (built on gaka)"
   :dependencies [[org.clojure/clojure "1.2.1"]
 		 [org.clojars.wilkes/gaka "0.2.2"]]
